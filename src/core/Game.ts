64f5d1f445d47c73b140c4e9287e4f5403429086
Linzisong <744740666@qq.com>
2024-08-07 23:34:48 +0800

    feat: 游戏开始类Game

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,6 +1,22 @@
 import { GameStatus } from "../types/common";
+import { SquareGroup } from "./SquareGroup";
 
 export class Game {
   // 游戏状态
   gameStatus: GameStatus = GameStatus.init 
+  // 当前方块
+  curTeris?: SquareGroup
+  // 下一个方块
+  nextTeris?: SquareGroup
+
+  // 游戏开始
+  start() {}
+
+  // 当前方块自由下落
+  private autoDrop() {
+
+  }
+
+  // 切换方块
+  private switchTeris() {}
 }
