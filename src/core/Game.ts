598f925303c28e20747adf3f05df90547ed06089
Linzisong <744740666@qq.com>
2024-08-14 22:35:25 +0800

    fix: game class

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,22 +1,24 @@
 import { GameStatus } from "../types/common";
 import { SquareGroup } from "./SquareGroup";
+import { createTeris } from "./Teris";
 
 export class Game {
   // 游戏状态
-  gameStatus: GameStatus = GameStatus.init 
+  gameStatus: GameStatus = GameStatus.init;
   // 当前方块
-  curTeris?: SquareGroup
+  curTeris?: SquareGroup;
   // 下一个方块
-  nextTeris?: SquareGroup
+  nextTeris: SquareGroup = createTeris({x: 0, y: 0});
 
   // 游戏开始
   start() {}
 
   // 当前方块自由下落
-  private autoDrop() {
-
-  }
+  private autoDrop() {}
 
   // 切换方块
-  private switchTeris() {}
+  private switchTeris() {
+    this.curTeris = this.nextTeris
+    this.nextTeris = createTeris({x: 0, y: 0});
+  }
 }
