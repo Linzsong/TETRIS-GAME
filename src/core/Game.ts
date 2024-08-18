1e8511a655708d1a67f9b0c4b006a41e6429aa21
Linzisong <744740666@qq.com>
2024-08-18 22:12:27 +0800

    feat: 设置中心坐标

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,4 +1,5 @@
 import { GameStatus, GameViewer, MoveDirection } from "../types/common";
+import GameConfig from "./GameConfig";
 import { SquareGroup } from "./SquareGroup";
 import { createTeris } from "./Teris";
 import { TerisRule } from "./TerisRule";
@@ -14,10 +15,9 @@ export class Game {
   private _duration: number = 1000;
 
   constructor(private _viewer: GameViewer) {
-    // this.resetCenterPoint(GameConfig.nextSize.width, this._nextTeris);
-    // this._viewer.showNext(this._nextTeris);
+    this.resetCenterPoint(GameConfig.nextSize.width, this._nextTeris);
+    this._viewer.showNext(this._nextTeris);
   }
-
 
   // 游戏开始
   start() {
@@ -33,20 +33,35 @@ export class Game {
 
   // 当前方块自由下落
   private autoDrop() {
-    if(this._timer || this._gameStatus === GameStatus.over) {
-      return
+    if (this._timer || this._gameStatus === GameStatus.over) {
+      return;
     }
     this._timer = setInterval(() => {
-      if(this._curTeris) {
-        TerisRule.move(this._curTeris, MoveDirection.down)
+      if (this._curTeris) {
+        TerisRule.move(this._curTeris, MoveDirection.down);
       }
-
-    }, this._duration)
+    }, this._duration);
   }
 
   // 切换方块
   private switchTeris() {
     this._curTeris = this._nextTeris;
     this._nextTeris = createTeris({ x: 0, y: 0 });
+  }
+
+  // 设置中心坐标
+  resetCenterPoint(width: number, teris: SquareGroup) {
+    const x = Math.ceil(width / 2) - 1;
+    const y = 0;
+    teris.centerPoint = { x, y };
+    while (teris.squares.some((it) => it.point.y < 0)) {
+      teris.squares.forEach(
+        (sq) =>
+          (sq.point = {
+            x: sq.point.x,
+            y: sq.point.y + 1,
+          })
+      );
+    }
   }
 }
