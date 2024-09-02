2ab13d7626fdaea96869f1066bb3acb1f6c77f38
Linzsong <744740666@qq.com>
2024-09-02 23:28:31 +0800

    fix: delete

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import { SquarePageViewer } from "./core/viewer/SquarePageViewer";
 import $ from "jquery";
-import { createTeris } from "./core/Teris";
-import { TerisRule } from "./core/TerisRule";
-import { MoveDirection } from "./types/common";
 import { Game } from "./core/Game";
 import { GamePageViewer } from "./core/viewer/GamePageViewer";
 
 
 var g = new Game(new GamePageViewer());
 
-// const group = createTeris({ x: 5, y: 5 })
-
-// group.squares.forEach(sq => {
-//   sq.iView = new SquarePageViewer(sq, $("#root"));
-// });
 
 $("#btnStart")[0].addEventListener("click", () => {
   g.start();
