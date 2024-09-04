bbccae0b78eef3d42693023c34376278630f34f8
Linzsong <744740666@qq.com>
2024-09-04 23:21:00 +0800

    fix: update

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,4 @@
-import $ from "jquery";
 import { Game } from "./core/Game";
 import { GamePageViewer } from "./core/viewer/GamePageViewer";
 
-
-var g = new Game(new GamePageViewer());
-
-
-$("#btnStart")[0].addEventListener("click", () => {
-  g.start();
-}, false);
-$("#btnPause")[0].addEventListener("click", () => {
-  g.pause();
-}, false);
-
-$("#up")[0].addEventListener("click", () => {
-  g.controlRotate();
-
-}, false);
-$("#down")[0].addEventListener("click", () => {
-  g.controlDown();
-}, false);
-
-$("#left")[0].addEventListener("click", () => {
-  g.controlLeft();
-
-}, false);
-$("#right")[0].addEventListener("click", () => {
-  g.controlRight();
-}, false);
+new Game(new GamePageViewer());
